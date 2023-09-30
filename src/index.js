b0091b7203c631a41909c3491337cf772119ecbc
Tapishagrawal <tapishg4@gmail.com>
2023-09-30 19:39:39 +0530

    done

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ root.render(
       </Provider>
     </BrowserRouter>
   </LoginPageContextProvider>
-  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
