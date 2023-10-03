7185f207521b928be907ab8c67d33f9bb0154d38
Tapishagrawal <tapishg4@gmail.com>
2023-10-03 08:13:24 +0530

    add cart page and slider with functionality

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -3,9 +3,11 @@ import { PiBasketBold } from 'react-icons/pi';
 import { MdDeleteOutline } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
 import { setDataInLocal, setDeletedWishListDataInLocal } from "../redux/localReducer/action"
+import { useNavigate } from 'react-router-dom';
 
 export const WishList = () => {
     const localWishListData = useSelector((store) => store.localReducer.wishListData);
+    const navigate = useNavigate()
     const dispatch = useDispatch();
 
     const handleDeleteCartData = (id) => {
@@ -24,7 +26,7 @@ export const WishList = () => {
                             <span onClick={() => handleDeleteCartData(data.id)} className='absolute bg-white p-2 rounded-full shadow-[0_8px_24px_rgba(149,157,165,0.4)] right-2 cursor-pointer hover:text-orange-600 transition-all duration-300'>
                                 <MdDeleteOutline />
                             </span>
-                            <div className='w-[90%] h-[250px] m-auto overflow-hidden'>
+                            <div onClick={() => navigate(`/singleProductWomen/${data.id}`)} className='w-[90%] h-[250px] m-auto overflow-hidden cursor-pointer'>
                                 <img width={"100%"} className='text-center rounded' src={data.image} alt="name" />
                             </div>
                             <div className='flex items-center justify-between px-2 mt-2'>
@@ -32,7 +34,7 @@ export const WishList = () => {
                                     <p className='font-semibold line-clamp-1'>{data.name}</p>
                                     <p className='font-medium'>&#x20B9;{data.price}</p>
                                 </div>
-                                <div onClick={()=>handleAddCard(data)} className='p-2 cursor-pointer text-lg rounded-full hover:bg-black hover:text-white transition duration-300 shadow-[0_8px_24px_rgba(149,157,165,0.2)]'>
+                                <div onClick={() => handleAddCard(data)} className='p-2 cursor-pointer text-lg rounded-full hover:bg-black hover:text-white transition duration-300 shadow-[0_8px_24px_rgba(149,157,165,0.2)]'>
                                     <PiBasketBold />
                                 </div>
                             </div>
