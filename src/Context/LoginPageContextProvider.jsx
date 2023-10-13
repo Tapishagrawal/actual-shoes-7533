3d274f517f3a1c473d46363b58959c6c74a8839e
Tapishagrawal <tapishg4@gmail.com>
2023-10-13 23:22:56 +0530

    done

--- src/Context/LoginPageContextProvider.jsx
+++ src/Context/LoginPageContextProvider.jsx
@@ -5,13 +5,15 @@ export const LoginPageContext = createContext();
 const LoginPageContextProvider = ({ children }) => {
     const [isLoginPageVisibaleVisible, setIsLoginPageVisible] = useState(false);
     const [adminAuth,setadminAuth] = useState(localStorage.getItem("adminAuth") || null)
+    const [singlUser,setSinglUser] = useState(localStorage.getItem("currentUser") || null)
 
     const handleToggleLoginPage = () => {
+        setSinglUser(true)
         setIsLoginPageVisible(!isLoginPageVisibaleVisible);
     }
 
     return (
-        <LoginPageContext.Provider value={{ isLoginPageVisibaleVisible, handleToggleLoginPage, adminAuth, setadminAuth }}>
+        <LoginPageContext.Provider value={{ isLoginPageVisibaleVisible, handleToggleLoginPage, adminAuth, setadminAuth, singlUser, setSinglUser }}>
             <div>
                 {children}
             </div>
